74e8954bc851560d1e8ec7150c340d12732e8ac6
Nick <nick.kim.work@gmail.com>
2025-09-13 11:44:10 +0200

    changin default home page to be profitable-deals

--- webapp/deal_board/urls.py
+++ webapp/deal_board/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.views.generic.base import RedirectView
 from . import views
 
 urlpatterns = [
-    path('', views.product_list_view, name='product_list'),
+    path('', RedirectView.as_view(url='/profitable-deals/', permanent=False), name='home'),
+    path('all-products/', views.product_list_view, name='product_list'),
     path('profitable-deals/', views.profitable_deals_view, name='profitable_deals'),
 ]
